5282c1026a0c02a41219f1a7b09614d602791b29
JesterXL <jesse@jessewarden.com>
2015-08-23 12:59:18 -0400

    got workout schedule

---
+++ src/api/workouts.js
@@ -0,0 +1,66 @@
+var _ = require('lodash');
+var defaultSchedule = require('./workouts/fixtures/defaultSchedule');
+
+function validDate(date)
+{
+	return date.toString() !== 'Invalid Date';
+}
+
+function goodDate(date)
+{
+	return _.isDate(date) && validDate(date);
+}
+
+function getDefaultWorkoutSchedule()
+{
+	return defaultSchedule;
+}
+
+function getNextWorkoutDay(day)
+{
+	day += 2;
+	if(day > 6)
+	{
+		return day -= 6;
+	}
+	return day;
+}
+
+var getThirdWorkoutDay = _.flow(getNextWorkoutDay, getNextWorkoutDay);
+
+function getWorkoutDays(day)
+{
+	return [day, getNextWorkoutDay(day), getThirdWorkoutDay(day)];
+}
+
+function getWorkoutSchedule(workoutDay, callback)
+{
+
+	if(goodDate(workoutDay) === false)
+	{
+		callback(new Error('Bad Date.'));
+	}
+	else
+	{
+		var startDay = workoutDay.getDay();
+		var workoutDays = getWorkoutDays(startDay);
+		defaultSchedule[0].days = workoutDays;
+		defaultSchedule[1].days = getRestDaysFromWorkoutDays(workoutDays);
+		callback(null, defaultSchedule);
+	}
+}
+
+function getRestDaysFromWorkoutDays(workoutDays)
+{
+	return _.difference([0, 1, 2, 3, 4, 5, 6], workoutDays);
+}
+
+var workouts = {
+	validDate: validDate,
+	goodDate: goodDate,
+	getDefaultWorkoutSchedule: getDefaultWorkoutSchedule,
+	getWorkoutSchedule: getWorkoutSchedule
+
+};
+
+module.exports = workouts;
---
+++ src/api/workouts/fixtures/squat.js
@@ -0,0 +1,20 @@
+{
+    name: 'Squat',
+    sets: [
+        {
+            reps: 5,
+            weight: 295,
+            actualReps: 5
+        },
+        {
+            reps: 5,
+            weight: 295,
+            actualReps: 5
+        },
+        {
+            reps: 5,
+            weight: 295,
+            actualReps: 5
+        }
+    ]
+}
