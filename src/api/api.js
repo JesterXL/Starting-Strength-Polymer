eb652ff597ccba0164d6911ff774471c3f003ff4
JesterXL <jesse@jessewarden.com>
2015-08-31 13:40:35 -0400

    login view seems wired up, now to create register, then I'll figure out this JWT insanity.

--- src/api/api.js
+++ src/api/api.js
@@ -16,6 +16,26 @@ api.pre(restify.CORS({
 api.pre(restify.fullResponse());
 api.use(restify.bodyParser());
 
+// var passport = require('passport');
+// var JwtStrategy = require('passport-jwt').Strategy;
+// var opts = {}
+// opts.secretOrKey = 'secret';
+// opts.issuer = "accounts.jessewarden.com";
+// opts.audience = "jessewarden.com";
+// passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+//     User.findOne({id: jwt_payload.sub}, function(err, user) {
+//         if (err) {
+//             return done(err, false);
+//         }
+//         if (user) {
+//             done(null, user);
+//         } else {
+//             done(null, false);
+//             // or you could create a new account
+//         }
+//     });
+// }));
+
 api.get('/ping', function (req, res, next) {
     console.log("ping called");
     res.send(200, {response: true});
@@ -24,6 +44,29 @@ api.get('/ping', function (req, res, next) {
 api.get('/', function(req, res) {
     console.log('default called');
     res.send(200);
+});
+
+// api.post('/profile', passport.authenticate('jwt', { session: false}),
+//     function(req, res)
+//     {
+//         console.log("*** /profile ***");
+//         console.log("user:", req.user);
+//         res.send(req.user.profile);
+//     }
+// );
+
+// api.post('/login',
+//   passport.authenticate('jwt', { session: false}),
+//   function(req, res) {
+//     // If this function gets called, authentication was successful.
+//     // `req.user` contains the authenticated user.
+//     res.json({response: true, user: req.user});
+//   });
+
+api.post('/login', function(req, res)
+{
+    console.log("*** /login ***");
+    res.json({result: true});
 });
 
 api.get('/api/workouts/today', function(req, res)
